e2c16930b64e53bd9b94e24fc7ece3f13f1b4f9c
Lapo Luchini <lapo@lapo.it>
2022-03-11 08:45:20 +0000

    First Playnite Prometheus/OpenMetrics exporter.

---
+++ exporter.psm1
@@ -0,0 +1,76 @@
+$textFile = 'C:\Program Files\windows_exporter\textfile_inputs\playnite.prom'
+$minPlay = 300
+
+function CalculateMetrics() {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        $game
+    )
+    Begin {
+        "# HELP playnite_game_total Total games."
+        "# TYPE playnite_game_total gauge"
+        "# HELP playnite_game_played_total Total games with at least 5 minutes of play time."
+        "# TYPE playnite_game_played_total gauge"
+        "# HELP playnite_game_playtime_seconds single game playtime in seconds."
+        "# TYPE playnite_game_playtime_seconds gauge"
+        "# HELP playnite_game_playtime_seconds_total Total playtime in seconds."
+        "# TYPE playnite_game_playtime_seconds_total gauge"
+        $num = 0
+        $numP = 0
+        $time = 0
+    }
+    Process {
+        ++$num
+        $time += $game.Playtime
+        $name = $game.Name -replace '([\\"])', '\$1'
+        if ($game.Playtime -ge $minPlay) {
+            ++$numP
+            "playnite_game_playtime_seconds{game=`"$name`"} $time"
+        }
+    }
+    End {
+        $__logger.Info("Calculated metrics: total $num played $numP playtime $time")
+        "playnite_game_total $num"
+        "playnite_game_played_total $numP"
+        "playnite_game_playtime_seconds_total $time"
+    }
+}
+
+function SaveMetrics() {
+    $PlayniteApi.Database.Games | Select Name, Source, Playtime | CalculateMetrics | Out-File $textFile -Encoding utf8
+}
+
+function OnApplicationStarted() { SaveMetrics }
+function OnApplicationStopped() { SaveMetrics }
+function OnLibraryUpdated() { SaveMetrics }
+function OnGameStopped() { SaveMetrics }
+
+function ExportLibrary() {
+    param(
+        $scriptMainMenuItemActionArgs
+    )
+    SaveMetrics
+    # $path = $PlayniteApi.Dialogs.SaveFile("OpenMetrics|*.prom|CSV|*.csv|Formatted TXT|*.txt")
+    # if ($path) {
+    #     if ($path -match ".prom$") {
+    #         $PlayniteApi.Database.Games | Select Name, Source, Playtime | CalculateMetrics | Out-File $path -Encoding utf8
+    #     } elseif ($path -match ".csv$") {
+    #         $PlayniteApi.Database.Games | Select Name, Source, ReleaseDate, Playtime, IsInstalled | ConvertTo-Csv | Out-File $path -Encoding utf8
+    #     } else {
+    #         $PlayniteApi.Database.Games | Select Name, Source, ReleaseDate, Playtime, IsInstalled | Format-Table -AutoSize | Out-File $path -Encoding utf8
+    #     }
+    #     $PlayniteApi.Dialogs.ShowMessage("Library exported successfully.");
+    # }
+}
+
+function GetMainMenuItems() {
+    param(
+        $menuArgs
+    )
+    $menuItem = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
+    $menuItem.Description = "Export OpenMetrics"
+    $menuItem.FunctionName = "ExportLibrary"
+    $menuItem.MenuSection = "@"
+    return $menuItem
+}
